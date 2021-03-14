2e37e7e2a87b1be274c33bc0c2ae29fb5087ab0a
Kavya-uday123 <mca522@rajagiri.edu>
2021-03-14 21:18:31 +0530

    Furniture

--- bedroom.php
+++ bedroom.php
@@ -10,23 +10,23 @@
 <body style="background-color:cadetblue">
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
-          <a class="navbar-brand" href="index.html">Home</a>
+          <a class="navbar-brand" href="index.php">Home</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="sofa.html">Sofa</a>
+                <a class="nav-link active" aria-current="page" href="sofa.php">Sofa</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="bedroom.html">Bedroom</a>
+                <a class="nav-link" href="bedroom.php">Bedroom</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="dining.html">Dining</a>
+                <a class="nav-link" href="dining.php">Dining</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="lightning.html">Lightning</a>
+                <a class="nav-link" href="lightning.php">Lightning</a>
               </li>
             </ul>
           </div>
